64300a67f66db5dd60e8f978cc8672d409766e54
sammyedy <samueledariese@gmail.com>
2024-06-02 01:53:01 +0100

    updated the clear dashboard to work properly

--- dash/delete.php
+++ dash/delete.php
@@ -1,5 +1,5 @@
 <?php
-    $filepath = "./user/log.txt";  // Assuming log.txt is in the same directory as the PHP file
+    $filepath = "../vote/log.txt";  // Assuming log.txt is in the same directory as the PHP file
 
     if (file_exists($filepath)) {
         unlink($filepath);  // Delete the file
